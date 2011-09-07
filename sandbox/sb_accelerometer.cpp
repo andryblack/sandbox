e2ddc4b1afd3d7b1e6d7da68b9d34c157d7e6ce1
AndryBlack <blackicebox@gmail.com>
2011-09-08 00:56:55 +0400

    accelerometer

---
+++ sandbox/sb_accelerometer.cpp
@@ -0,0 +1,55 @@
+/*
+ *  sb_accelerometer.cpp
+ *  YinYang
+ *
+ *  Created by Андрей Куницын on 07.09.11.
+ *  Copyright 2011 AndryBlack. All rights reserved.
+ *
+ */
+
+#include "sb_accelerometer.h"
+#include <ghl_system.h>
+#include "sb_bind.h"
+
+namespace Sandbox {
+	
+	static size_t enable_count = 0;
+	
+	Accelerometer::Accelerometer( GHL::System* system ) : m_system( system ) {
+		m_data = Vector3f( 0,0,0);
+		bool enable = true;
+		m_system->SetDeviceState( GHL::DEVICE_STATE_ACCELEROMETER_ENABLED, &enable );
+		enable_count++;
+	}
+	Accelerometer::~Accelerometer() {
+		if (enable_count) {
+			enable_count--;
+			if (!enable_count) {
+				bool enable = false;
+				m_system->SetDeviceState( GHL::DEVICE_STATE_ACCELEROMETER_ENABLED, &enable );
+			}
+		}
+	}
+	bool Accelerometer::Update( float dt ) {
+		float data[3];
+		if (m_system->GetDeviceData( GHL::DEVICE_DATA_ACCELEROMETER, data )) {
+			m_data.x = data[0];
+			m_data.y = data[1];
+			m_data.z = data[2];
+		}
+		return false;
+	}
+	
+	void Accelerometer::Bind( Lua* lua ) {
+		SB_BIND_BEGIN_SHARED_SUBCLASS(Sandbox::Accelerometer,Sandbox::Thread)
+		SB_BIND_NO_CONSTRUCTOR
+		SB_BIND_BEGIN_METHODS
+		SB_BIND_END_METHODS
+		SB_BIND_BEGIN_PROPERTYS
+		SB_BIND_PROPERTY_RO(Sandbox::Accelerometer,Data,GetData,Sandbox::Vector3f)
+		SB_BIND_END_PROPERTYS
+		SB_BIND_END_CLASS
+		SB_BIND(lua)
+	}
+	
+}
---
+++ sandbox/sb_accelerometer.h
@@ -0,0 +1,38 @@
+/*
+ *  sb_accelerometer.h
+ *  YinYang
+ *
+ *  Created by Андрей Куницын on 07.09.11.
+ *  Copyright 2011 AndryBlack. All rights reserved.
+ *
+ */
+
+#ifndef SB_ACCELEROMETER_H_INCLUDED
+#define SB_ACCELEROMETER_H_INCLUDED
+
+#include "sb_thread.h"
+#include "sb_vector3.h"
+
+namespace GHL {
+	struct System;
+}
+
+namespace Sandbox {
+	
+	class Lua;
+	
+	class Accelerometer : public Thread {
+	public:
+		explicit Accelerometer( GHL::System* system );
+		~Accelerometer();
+		bool Update( float dt );
+		const Vector3f& GetData() const { return m_data;}
+		static void Bind( Lua* lua );
+	private:
+		GHL::System*	m_system;
+		Vector3f		m_data;
+	};
+	typedef shared_ptr<Accelerometer> AccelerometerPtr;
+}
+
+#endif /*SB_ACCELEROMETER_H_INCLUDED*/
