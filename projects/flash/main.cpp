bab45f70e3879c74d2dd21ab0811805574895844
Andrey Kunitsyn <blackicebox@gmail.com>
2015-04-17 04:11:09 +0300

    fix flash main

--- projects/flash/main.cpp
+++ projects/flash/main.cpp
@@ -1,8 +1,6 @@
-#include "../../src/application.h"
 
-void register_vk_auth(lua_State* L){}
+int StartApplication(int argc,char** argv);
 
 int main(int argc,char** argv) {
-    Application* app = new Application();
-    return GHL_StartApplication(app,argc,argv);
+    return StartApplication(argc,argv);
 }
