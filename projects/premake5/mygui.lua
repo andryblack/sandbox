01898bcd0b1469be213b76a761d128a0d1bcfc3c
Andrey Kunitsyn <blackicebox@gmail.com>
2015-06-20 01:49:03 +0300

    extract MyGUI project

---
+++ projects/premake5/mygui.lua
@@ -0,0 +1,23 @@
+project 'MyGUI'
+
+	kind 'StaticLib'
+
+	targetdir (_WORKING_DIR .. '/lib/' .. platform_dir)
+
+	targetname ('MyGUI_' .. platform_dir)
+
+	files {
+		sandbox_dir .. '/MyGUI/MyGUIEngine/**.h',
+		sandbox_dir .. '/MyGUI/MyGUIEngine/**.cpp'
+	}
+
+	includedirs {
+		sandbox_dir .. '/MyGUI/MyGUIEngine/include',
+		sandbox_dir .. '/include',
+		sandbox_dir .. '/freetype/include'
+	}
+
+	defines 'MYGUI_CONFIG_INCLUDE="<../../../sandbox/mygui/sb_mygui_config.h>"'
+
+	configuration "Debug"
+			targetsuffix "_d"
