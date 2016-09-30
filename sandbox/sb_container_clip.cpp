84251956041a13dec99975e8df40bb5acb565d4f
Andrey Kunitsyn <blackicebox@gmail.com>
2016-09-30 14:53:16 +0300

    fix clip

--- sandbox/sb_container_clip.cpp
+++ sandbox/sb_container_clip.cpp
@@ -14,13 +14,18 @@ namespace Sandbox {
         Recti nclip = m_rect;
         Vector2f lt = tr.transform(nclip.GetTopLeft());
         Vector2f rb = tr.transform(nclip.GetBottomRight());
-        nclip.x = std::min(lt.x, rb.x);
-        nclip.y = std::min(lt.y, rb.y);
+        nclip.x = std::max(0.0f,std::min(lt.x, rb.x));
+        nclip.y = std::max(0.0f,std::min(lt.y, rb.y));
         nclip.w = std::max(lt.x, rb.x) - nclip.x;
         nclip.h = std::max(lt.y, rb.y) - nclip.y;
-        g.SetClipRect(nclip);
-        Container::Draw(g);
-        g.SetClipRect(clip);
+        if (nclip.w > 0 && nclip.h > 0) {
+            nclip = nclip.GetIntersect(clip);
+            if (nclip.w > 0 && nclip.h > 0) {
+                g.SetClipRect(nclip);
+                Container::Draw(g);
+                g.SetClipRect(clip);
+            }
+        }
     }
     
 }
