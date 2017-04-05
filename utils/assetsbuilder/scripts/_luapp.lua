857fe282fb35cbf62c3093e8f16908ccaba54398
andryblack <blackicebox@gmail.com>
2017-04-05 13:07:08 +0300

    Store lines at preprocess

--- utils/assetsbuilder/scripts/_luapp.lua
+++ utils/assetsbuilder/scripts/_luapp.lua
@@ -6,16 +6,21 @@ function M.preprocess(file,defs,fn)
   local chunk = {n=0}
   local funcs = {}
   local output = {n=0}
-  function funcs._put(s)
+  function funcs._put(l,s)
+    while(#output<l) do
+      table.insert(output,'--')
+    end
     table.insert(output,s)
   end
   local mt = setmetatable(funcs,{__index=defs})
+  local line_n = 0
   for line in file:lines() do
      if string.find(line, "^#") then
       table.insert(chunk, string.sub(line, 2))
      else
-      table.insert(chunk,string.format('_put %q ', line))
+      table.insert(chunk,string.format('_put (%d,%q) ',line_n, line))
     end
+    line_n = line_n + 1
   end
   assert(load(table.concat(chunk,'\n'),'pp:'..(fn or 'file'),'t',mt))()
   return table.concat(output,'\n')
