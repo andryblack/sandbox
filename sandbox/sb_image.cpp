3faf2e2d8452c02261d428894a6a7659e83edaab
andryblack <blackicebox@gmail.com>
2017-05-24 17:03:51 +0300

    Fix CheckBit for rotated image

--- sandbox/sb_image.cpp
+++ sandbox/sb_image.cpp
@@ -4,18 +4,27 @@
 namespace Sandbox {
     
     bool Image::CheckBit(int x,int y,Resources* resources) {
-        x+=int(m_hotspot.x*m_width/m_src_w);
+        float src_w = m_rotated ? m_src_h : m_src_w;
+        float src_h = m_rotated ? m_src_w : m_src_h;
+        x+=int(m_hotspot.x*m_width/src_w);
         if (x<0)
             return false;
-        y+=int(m_hotspot.y*m_height/m_src_h);
+        y+=int(m_hotspot.y*m_height/src_h);
         if (y<0)
             return false;
         if (x>m_width)
             return false;
         if (y>m_height)
             return false;
-        x = int(m_src_x + x * m_src_w / m_width);
-        y = int(m_src_y + y * m_src_h / m_height);
+        if (m_rotated) {
+            float ox = x;
+            float oy = y;
+            x = int(m_src_x + oy * m_src_w / m_height);
+            y = int(m_src_y + m_src_h - ox * m_src_h / m_width);
+        } else {
+            x = int(m_src_x + x * m_src_w / m_width);
+            y = int(m_src_y + y * m_src_h / m_height);
+        }
         if (!m_texture) return false;
         BitmaskPtr bm = m_texture->GetBitmask(resources);
         if (!bm)
