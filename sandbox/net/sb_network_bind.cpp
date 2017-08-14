ff0409d4038fe7b9d527cb51482b442ef6e97bfa
andryblack <blackicebox@gmail.com>
2017-08-14 20:49:41 +0300

    bind network

--- sandbox/net/sb_network_bind.cpp
+++ sandbox/net/sb_network_bind.cpp
@@ -27,6 +27,9 @@ SB_META_END_KLASS_BIND()
 SB_META_BEGIN_KLASS_BIND(Sandbox::NetworkDataRequest)
 SB_META_END_KLASS_BIND()
 
+SB_META_BEGIN_KLASS_BIND(Sandbox::NetworkFileRequest)
+SB_META_END_KLASS_BIND()
+
 #ifndef SB_NO_RESOURCES
 SB_META_BEGIN_KLASS_BIND(Sandbox::ImageRequest)
 SB_META_PROPERTY_RO(Image,GetImage)
@@ -47,6 +50,7 @@ SB_META_END_KLASS_BIND()
 SB_META_BEGIN_KLASS_BIND(Sandbox::Network)
 SB_META_METHOD(SimpleGET)
 SB_META_METHOD(SimplePOST)
+SB_META_METHOD(GETFile)
 SB_META_METHOD(POST)
 SB_META_METHOD(SetCookie);
 #ifndef SB_NO_RESOURCES
@@ -60,6 +64,7 @@ namespace Sandbox {
         luabind::ExternClass<Sandbox::NetworkRequestBase>(vm->GetVM());
         luabind::ExternClass<Sandbox::NetworkRequest>(vm->GetVM());
         luabind::ExternClass<Sandbox::NetworkDataRequest>(vm->GetVM());
+        luabind::ExternClass<Sandbox::NetworkFileRequest>(vm->GetVM());
         luabind::ExternClass<Sandbox::NetworkPostData>(vm->GetVM());
         luabind::Class<Sandbox::NetworkMultipartFormData>(vm->GetVM());
 #ifndef SB_NO_RESOURCES
